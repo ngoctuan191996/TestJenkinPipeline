bbc9d10dd33886e189470a0ec306ceb7fc01e912
ngoctuan191996 <ngoctuan191996@gmail.com>
2017-01-08 00:53:11 +0700

    push

--- test-jenkins-pipeline/src/main/java/dnt/test/jenkins/pipeline/MainApplication.java
+++ test-jenkins-pipeline/src/main/java/dnt/test/jenkins/pipeline/MainApplication.java
@@ -3,6 +3,6 @@ package dnt.test.jenkins.pipeline;
 public class MainApplication {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-        System.out.println("Hlo");
+        System.out.println("");
     }
 }
