7987d0f098c8cb92483e8af49e3d42f70e0c2caf
ngoctuan191996 <ngoctuan191996@gmail.com>
2017-01-10 00:35:21 +0700

    adf

---
+++ test-jenkins-pipeline/src/main/java/dnt/test/jenkins/pipeline/MainApplication (3rd copy).java
@@ -0,0 +1,8 @@
+package dnt.test.jenkins.pipeline;
+
+public class MainApplication {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.out.println("");
+    }
+}
